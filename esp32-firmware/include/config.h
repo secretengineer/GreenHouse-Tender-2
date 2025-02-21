f04cd8be0ea8d5800b6847a44468ad596d3ae279
Patrick Ryan <secretengineer@gmail.com>
2025-02-21 09:23:43 -0700

    Remove sensitive ESP32 firmware config and add template

--- esp32-firmware/include/config.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-const char* ssid = "your-ssid";
-const char* password = "your-password";
-const char* mqtt_server = "your-cluster.hivemq.cloud";
-const int mqtt_port = 8883;
-const char* mqtt_user = "your-username";
-const char* mqtt_pass = "your-password";
-#endif
